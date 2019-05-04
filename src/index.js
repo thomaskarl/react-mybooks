d52590cab303d7cdefde44fc15ead42fa8dc7827
Thomas Karl <thomaskarlandersson@outlook.com>
2019-05-04 21:09:01 +0200

    Added homepage, so build was able to output content

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Library from './components/Library';
 
-ReactDOM.render(<Library/>, document.getElementById('root'));
+ReactDOM.render(<Library/>, document.getElementById('myBooks'));
 
 serviceWorker.unregister();
